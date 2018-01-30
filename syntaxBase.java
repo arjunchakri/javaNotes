64ba2070404d9b66a845e4b918076e80726ebc62
Arjun Chakravarthy Nunugonda <arjunchakri@gmail.com>
2018-01-30 17:16:46 +0530

    Update syntaxBase.java

--- syntaxBase.java
+++ syntaxBase.java
@@ -1,23 +1,26 @@
-//get filenames and READ at root and with subfolders of the directory
-    PATH------------------>static path = "C:\\Users\\anunugonda\\Desktop\\datasets";
-		public static void listAllFiles(String path){
+
+	// get filenames root and with subfolders of the directory
+	PATH------------------>static path="C:\\Users\\anunugonda\\Desktop\\datasets";
+
+	public static void listAllFiles(String path) {
 		File file = new File(path);
 		File[] filesList = file.listFiles();
-		for(File x : filesList){
-			if(x.isFile()){
-				System.out.println("File path => "+ path + "\\" + x.getName());
-        //READ
-        //        FileInputStream fin = new FileInputStream(path + "\\" + x.getName());
-        //				Scanner sc = new Scanner(fin);
-        //				while(sc.hasNextLine()){
-        //					System.out.println(sc.nextLine());
-        //				}				
+		for (File x : filesList) {
+			if (x.isFile()) {
+				System.out.println("File path => " + path + "\\" + x.getName());
 			}
-			if(x.isDirectory()){
-				//directory
-				//System.out.println("DIrec ---------" + path +"\\"+ x.getName());
-				listAllFiles(path +"\\"+ x.getName());
+			if (x.isDirectory()) {
+				// directory
+				// System.out.println("DIrec ---------" + path +"\\"+
+				// x.getName());
+				listAllFiles(path + "\\" + x.getName());
 			}
 		}
 	}
 
+	// reading a file basic
+	FileInputStream fin = new FileInputStream("C:\\Users\\anunugonda\\Desktop\\datasets\\abc.xml");
+	Scanner sc = new Scanner(fin);while(sc.hasNextLine())
+	{
+	    	System.out.println(sc.nextLine());
+	}

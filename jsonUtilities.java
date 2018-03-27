8ebe7b0197a5ad96d4d6d00a9588e90da2cc75a7
Arjun Chakravarthy Nunugonda <arjunchakri@gmail.com>
2018-03-27 16:56:50 +0530

    Update jsonUtilities.java

--- jsonUtilities.java
+++ jsonUtilities.java
@@ -6,19 +6,20 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-//object to JSON
-Gson gson = new Gson();
-StudentData student = new StudentData("abc", "def", "507");
-String json = gson.toJson(student);
+//converstion
+    //object to JSON
+    Gson gson = new Gson();
+    StudentData student = new StudentData("abc", "def", "507");
+    String json = gson.toJson(student);
 
-//Json to object
-Gson gson = new Gson();
-String json = getJsonString();
+    //Json to object
+    Gson gson = new Gson();
+    String json = getJsonString();
 
-StudentData student = gson.toJson(json, StudentData.class);
-//or
-Type type = new TypeToken < StudentData > () {}.getType();
-StudentData student = gson.toJson(json, type);
+    StudentData student = gson.toJson(json, StudentData.class);
+    //  or
+    Type type = new TypeToken < StudentData > () {}.getType();
+    StudentData student = gson.toJson(json, type);
 
 
 
@@ -26,21 +27,24 @@ StudentData student = gson.toJson(json, type);
 JsonObject jsonobj = new JsonParser().parse(results).getAsJsonObject();
 
 //selecting a child with name
-//object direct-chile ( "keyname":{ "some":"thing" })
-JsonObject jsonobj = jsonobj.getAsJsonObject("keyname");
-String entireJsondom = jsonobj.toString();
+    //object direct-chile ( "keyname":{ "some":"thing" })
+    JsonObject jsonobj = jsonobj.getAsJsonObject("keyname");
+    String entireJsondom = jsonobj.toString();
 
-//selecting direct-child value( "keyname":"thisvalue" ) 
-JsonElement element = jsonobject.get("keyname");
-String value = element.toString();
+    //selecting direct-child value( "keyname":"thisvalue" ) 
+    JsonElement element = jsonobject.get("keyname");
+    String value = element.toString();
 
-//array direct-child ( "keyname" : ["abcd","abbc",{"some" : "thing"}] )
-JsonArray array = jsonobj.getAsJsonArray("keyname");
-for (int i = 0; i < array.size(); i++) {
-    JsonElement element = array.get(i);
-    if (element.isJsonNull()) {
+    //array direct-child ( "keyname" : ["abcd","abbc",{"some" : "thing"}] )
+    JsonArray array = jsonobj.getAsJsonArray("keyname");
+    for (int i = 0; i < array.size(); i++) {
+        JsonElement element = array.get(i);
+        if (element.isJsonNull()) {
 
-    } else {
-        element.toString()
+        } else {
+            element.toString()
+        }
     }
-}
+
+
+

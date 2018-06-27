13b80c0c2f3c1db642e8d50f785cf63fdaa6f425
Arjun Chakravarthy Nunugonda <arjunchakri@gmail.com>
2018-06-27 15:02:27 +0530

    Update Collections.java

--- Collections.java
+++ Collections.java
@@ -1,7 +1,32 @@
 Arraylist:
     
-    a)
-    List<String> arrayList1 = new ArrayList<>();
+    a)iterating
+		List<Employee> employeeList =new ArrayList<Employee>();
+		employeeList.add(new Employee("01", "name1", "a", "nowhere"));
+		employeeList.add(new Employee("01", "name1", "a", "nowhere"));
+		employeeList.add(new Employee("01", "name1", "a", "nowhere"));
+		employeeList.add(new Employee("01", "name1", "a", "nowhere"));
+		employeeList.add(new Employee("01", "name1", "a", "nowhere"));
+		//(foreach)
+		for(Employee each : employeeList){
+			System.out.println(each.getName());
+		}
+
+		//iterator
+		Iterator iterator = employeeList.iterator();
+		while (iterator.hasNext()) {
+			Employee each = (Employee) iterator.next();
+			if (search(each.toString(), term)) {
+				System.out.println(each.toString());
+			}
+		}
+	public static boolean search(String source, String searchTerm) {
+		return source.toLowerCase().contains(searchTerm.toLowerCase());
+	}
+
+    
+	b)sorting-comparision
+	    	List<String> arrayList1 = new ArrayList<>();
 		arrayList1.add("one");
 		arrayList1.add("two");
 		arrayList1.add("three");
@@ -15,6 +40,7 @@ Arraylist:
 		Collections.sort(arrayList1);
 		Collections.sort(arrayList2);
 		
-    System.out.println(arrayList1.equals(arrayList2));
+    		System.out.println(arrayList1.equals(arrayList2));
+	
     
  
